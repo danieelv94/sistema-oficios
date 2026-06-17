734116c1a5b287ac0e861fe76cf9ddc19f20c3ff
Daniel Lopez vega <daniel_lopezvega@hotmail.com>
2026-06-17 16:51:21 -0600

    Al inhabilitar un director y asignar un nuevo director ya aparece en los oficios de comision y cambiar lo de encargada nancy

--- app/Models/Comision.php
+++ app/Models/Comision.php
@@ -8,8 +8,8 @@ class Comision extends Model
     use HasFactory;
     protected $guarded = []; // Esto ya permite todos los campos
 
-    public function user() { return $this->belongsTo(User::class); }
-    public function jefeArea() { return $this->belongsTo(User::class, 'jefe_area_id'); }
+    public function user() { return $this->belongsTo(User::class)->withTrashed(); }
+    public function jefeArea() { return $this->belongsTo(User::class, 'jefe_area_id')->withTrashed(); }
     public function vehiculo() { return $this->belongsTo(Vehiculo::class); }
     public function proyecto() { return $this->belongsTo(Proyecto::class); }
     public function unidadAdministrativa()
